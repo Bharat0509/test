8432985086e0a46fa82528290defb2b597f95e0c
Hardik Bhammar <94770400+Hardik8491@users.noreply.github.com>
2023-06-23 18:28:22 +0530

    Portfolio project intintiated

---
+++ src/Components/Home/Home.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import FeaturedArea from '../FeaturedArea/FeaturedArea'
+import Service from '../Service/Service'
+import Education from '../Educations/Education'
+import AmzingWork from '../MyAmzingWorks/AmzingWork'
+import Work from '../WorkProjects/Work'
+import Footer from '../Footer/Footer'
+import Contect from '../Contects/Contect'
+import About from '../AboutSection/AboutSection'
+
+const HomePage = () => {
+  return (
+    <div className="bg-white h-screen ">
+   <FeaturedArea/>
+   <About/>
+    
+   <Service/> 
+   <Education/>
+   <AmzingWork/>
+   <Work/>
+   <Contect/>
+   <Footer/>
+
+       </div>
+  )
+}
+
+export default HomePage
---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import About from '@/Components/AboutSection/AboutSection'
+import Header from '@/Components/Header/Header'
+import HomePage from '@/Components/Home/Home'
+
+
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="Main">
+ 
+           <HomePage/>
+                 
+  
+    
+    </main>
+  )
+}
