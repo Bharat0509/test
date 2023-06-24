51ad041d2cfdbb43835449375c4e401ecc424961
Bharat Bhammar <bhartbhammar3336@gmail.com>
2023-06-24 07:07:41 +0530

    2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 export default function Home() {
     return (
         <main className='Main'>
-            <h1>Hello workd</h1>
+            <h1 className='text-4xl bg-red-600'>Hello workd</h1>
 
             <HomePage />
         </main>
