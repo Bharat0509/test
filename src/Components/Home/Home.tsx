47b2b30f8f0c520759413356f39a0fd2fca499b4
Bharat Bhammar <bhartbhammar3336@gmail.com>
2023-06-24 07:10:19 +0530

    3

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,28 +1,27 @@
-import React from 'react'
-import FeaturedArea from '../FeaturedArea/FeaturedArea'
-import Service from '../Service/Service'
-import Education from '../Educations/Education'
-import AmzingWork from '../MyAmzingWorks/AmzingWork'
-import Work from '../WorkProjects/Work'
-import Footer from '../Footer/Footer'
-import Contect from '../Contects/Contect'
-import About from '../AboutSection/AboutSection'
+import React from "react";
+import FeaturedArea from "../FeaturedArea/FeaturedArea";
+import Service from "../Service/Service";
+import Education from "../Educations/Education";
+import AmzingWork from "../MyAmzingWorks/AmzingWork";
+import Work from "../WorkProjects/Work";
+import Footer from "../Footer/Footer";
+import Contect from "../Contects/Contect";
+import About from "../AboutSection/AboutSection";
 
 const HomePage = () => {
-  return (
-    <div className="bg-white h-screen ">
-    <FeaturedArea/>
-   {/* <About/> */}
-{/*     
+    return (
+        <div className='bg-white h-screen '>
+            <FeaturedArea />
+            <About />
+            {/*     
    <Service/> 
    <Education/>
    <AmzingWork/>
    <Work/>
    <Contect/>
    <Footer/>  */}
+        </div>
+    );
+};
 
-       </div>
-  )
-}
-
-export default HomePage
+export default HomePage;
